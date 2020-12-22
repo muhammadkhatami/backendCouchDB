32a2a968fd6ba246c03fcb2cf9479bed6122600c
Muhammad Khatami <khatami2121@gmail.com>
2020-12-22 17:49:49 +0700

    initial commit

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/muhammadkhatami/Documents/files/UI/semester7/pdb/backendCouchDB/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
